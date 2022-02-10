f75b19dea0cd4f76d1e8898934ad51ac87c6e404
Jerson Satoru Uyekita <jersonsatoru@yahoo.com.br>
2022-02-09 21:18:41 -0300

    feat: add movie data and movie models

---
+++ internal/data/user.go
@@ -0,0 +1,162 @@
+package data
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"time"
+
+	"github.com/jersonsatoru/lets-go-further/internal/validator"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  password  `json:"-"`
+	Activated bool      `json:"activated"`
+	Version   int       `json:"-"`
+}
+
+type password struct {
+	plaintext *string
+	hash      []byte
+}
+
+var (
+	ErrDuplicateEmail = errors.New("duplicate email")
+)
+
+func (p *password) Set(plaintextPassword string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintextPassword), 12)
+	if err != nil {
+		return err
+	}
+	p.plaintext = &plaintextPassword
+	p.hash = hash
+	return nil
+}
+
+func (p *password) Matches(plaintextPassword string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plaintextPassword))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+	return true, nil
+}
+
+func ValidateUser(v *validator.Validator, u *User) {
+	v.Check(len(u.Name) > 0, "name", "must be greater than 0")
+	v.Check(len(u.Name) <= 500, "name", "must have the maximum of 500 characters")
+	ValidateEmail(v, u.Email)
+	ValidatePasswordPlaintext(v, *u.Password.plaintext)
+	if u.Password.hash != nil {
+		panic("missing password hash for user")
+	}
+}
+
+func ValidateEmail(v *validator.Validator, email string) {
+	v.Check(email != "", "email", "must be greater than 0")
+	v.Check(validator.Matches(email, validator.EmailRxp), "email", "must be a valid email")
+}
+
+func ValidatePasswordPlaintext(v *validator.Validator, plaintextPassword string) {
+	v.Check(plaintextPassword != "", "password", "must not be empty")
+	v.Check(len(plaintextPassword) >= 8, "password", "must greater than 8")
+	v.Check(len(plaintextPassword) <= 72, "password", "must have maximum of 72 characters")
+}
+
+type UserModel struct {
+	DB *sql.DB
+}
+
+func (m *UserModel) Insert(user User) error {
+	query := `
+		INSERT INTO users (name, email, password_hash, activated)
+		VALUES ($1, $2, $3, $4)
+		RETURNING created_at, version, id
+	`
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	args := []interface{}{
+		user.Name,
+		user.Email,
+		user.Password.hash,
+		user.Activated}
+
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&user.CreatedAt, &user.Version, &user.ID)
+	if err != nil {
+		switch {
+		case err.Error() == `pq: duplicate key value violates unique constraint "users_email_key"`:
+			return ErrDuplicateEmail
+		default:
+			return err
+		}
+	}
+	return nil
+}
+
+func (m *UserModel) GetByEmail(email string) (*User, error) {
+	query := `
+		SELECT id, name, email, activated, version, created
+		FROM users
+		WHERE email = $1
+	`
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	var user User
+	err := m.DB.QueryRowContext(ctx, query, email).Scan(
+		&user.ID,
+		&user.Name,
+		&user.Email,
+		&user.Activated,
+		&user.Version,
+		&user.CreatedAt)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+	return &user, nil
+}
+
+func (m *UserModel) Update(user User) error {
+	query := `
+		UPDATE users
+		SET name = $1, email = $2, activated = $3, password = $4, version = version + 1
+		WHERE id = $5 AND version = $6
+		RETURNING version
+	`
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	args := []interface{}{
+		&user.Name,
+		&user.Email,
+		&user.Activated,
+		&user.Password.hash,
+		&user.ID,
+		&user.Version,
+	}
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&user.Version)
+	if err != nil {
+		switch {
+		case err.Error() == `pq: duplicate key value violates unique constraint "users_email_key"`:
+			return ErrDuplicateEmail
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+	return nil
+}
